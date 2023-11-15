c18c9ab08160369360963ce9ef681fa4240a72e7
XeviDev <jv.martinez.mellado@gmail.com>
2023-11-15 23:36:11 +0100

    added DI for the service

--- app/src/main/java/com/xevidev/rickandmortyapp/data/core/CharactersService.kt
+++ app/src/main/java/com/xevidev/rickandmortyapp/data/core/CharactersService.kt
@@ -3,11 +3,12 @@ package com.xevidev.rickandmortyapp.data.core
 import com.xevidev.rickandmortyapp.data.models.CharactersModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class CharactersService {
+class CharactersService @Inject constructor(private val apiClient: CharactersApiClient) {
     suspend fun getCharactersList(): List<CharactersModel> {
         return withContext(Dispatchers.IO) {
-            val response = api.getCharacters()
+            val response = apiClient.getCharacters()
             response.body()?: emptyList()
         }
     }
