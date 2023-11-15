0c8621a68a6346b9bd987a52e9c3bfaca72482fe
XeviDev <jv.martinez.mellado@gmail.com>
2023-11-15 23:24:51 +0100

    refactor and implementation of the repository

--- app/src/main/java/com/xevidev/rickandmortyapp/data/RickMortyRepository.kt
+++ app/src/main/java/com/xevidev/rickandmortyapp/data/RickMortyRepository.kt
@@ -1,4 +1,14 @@
 package com.xevidev.rickandmortyapp.data
 
+import com.xevidev.rickandmortyapp.data.models.CharactersModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class RickMortyRepository {
+    suspend fun getCharactersList(): List<CharactersModel> {
+        return withContext(Dispatchers.IO) {
+            val response = api.getCharacters()
+            response.body()?: emptyList()
+        }
+    }
 }
