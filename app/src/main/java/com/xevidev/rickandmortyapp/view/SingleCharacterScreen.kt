9925a3dfb09660c322636ae8adaea0dae72b4b21
XeviDev <jv.martinez.mellado@gmail.com>
2023-11-28 19:52:27 +0100

    adding img to single view

--- app/src/main/java/com/xevidev/rickandmortyapp/view/SingleCharacterScreen.kt
+++ app/src/main/java/com/xevidev/rickandmortyapp/view/SingleCharacterScreen.kt
@@ -1,7 +1,10 @@
 package com.xevidev.rickandmortyapp.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -13,6 +16,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import coil.compose.AsyncImage
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.xevidev.rickandmortyapp.domain.model.CharacterDomain
@@ -21,16 +25,41 @@ import com.xevidev.rickandmortyapp.domain.model.CharacterDomain
 @Composable
 fun SingleCharacterScreen(
     navigator: DestinationsNavigator,
-    character:CharacterDomain
-    ) {
+    character: CharacterDomain
+) {
     Column(Modifier.fillMaxSize()) {
         TopBar(navigator = navigator)
+        ContentCharacter(character)
     }
+}
+
+@Composable
+fun ContentCharacter(character: CharacterDomain) {
+    CharacterImage(character.image)
+    CharacterInfo(character)
+}
+
+@Composable
+fun CharacterInfo(character: CharacterDomain) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Blue)
+    )
+}
+
+@Composable
+fun CharacterImage(image: String) {
+    AsyncImage(
+        model = image,
+        contentDescription = "character img",
+        modifier = Modifier.fillMaxWidth()
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar(navigator: DestinationsNavigator){
+fun TopBar(navigator: DestinationsNavigator) {
     CenterAlignedTopAppBar(title = { Text(text = "BeerApp") },
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
             containerColor = Color.Red,
