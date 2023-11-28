585ddfb93e531950536cfea5091b62e9288bb412
XeviDev <jv.martinez.mellado@gmail.com>
2023-11-28 19:31:18 +0100

    added topbar with edit text and fav button

--- app/src/main/java/com/xevidev/rickandmortyapp/view/CharactersListScreen.kt
+++ app/src/main/java/com/xevidev/rickandmortyapp/view/CharactersListScreen.kt
@@ -1,18 +1,26 @@
 package com.xevidev.rickandmortyapp.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,10 +40,13 @@ fun CharactersListScreen(
     charactersViewModel.getCharacters()
     val characters by charactersViewModel.characters.collectAsState()
 
-    LazyColumn {
+    Column {
+        TopBar(charactersViewModel = charactersViewModel)
+        LazyColumn {
 
-        items(characters) { character ->
-            CharacterCard(character = character)
+            items(characters) { character ->
+                CharacterCard(character = character)
+            }
         }
     }
 }
@@ -53,24 +64,39 @@ fun CharacterCard(character: CharacterDomain) {
                 contentDescription = null,
             )
             Column {
-               DataCard(character = character)
+                DataCard(character = character)
             }
         }
     }
 }
 
 @Composable
-fun DataCard(character: CharacterDomain){
-    Text(text = character.name,
+fun DataCard(character: CharacterDomain) {
+    Text(
+        text = character.name,
         modifier = Modifier.padding(8.dp),
         fontWeight = FontWeight.ExtraBold,
         fontSize = 16.sp
     )
-    Text(text = character.status+"-"+character.species,
-        modifier = Modifier.padding(8.dp)) 
+    Text(
+        text = character.status + "-" + character.species,
+        modifier = Modifier.padding(8.dp)
+    )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar(charactersViewModel: CharactersViewModel){
-    TopAppBar(title = { /*TODO*/ })
+fun TopBar(charactersViewModel: CharactersViewModel) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Red)
+    ) {
+        Row {
+            TextField(value = "test", onValueChange = {})
+            IconButton(onClick = { /*TODO*/ }, ) {
+                Icon(imageVector = Icons.Default.Favorite, contentDescription = "Favorite")
+            }
+        }
+    }
 }
